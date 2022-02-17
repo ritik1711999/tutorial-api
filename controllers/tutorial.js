fae7d48a958bc4f96ece1f0232337800936cf4b7
Ritik Maheshwari <ritikmaheshwari1711999@gmail.com>
2022-02-17 17:48:51 +0530

    checking hooks again 1

--- controllers/tutorial.js
+++ controllers/tutorial.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllTuts = async (req, res) => {
-  const tutorials = await Tutorial.findAll({
+  tutorials = await Tutorial.findAll({
     include: [
       {
         model: User,
